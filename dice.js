6ce211c7e21ab43a0d549203bde4683e89033968
enaito <emily.naito@tufts.edu>
2016-09-11 14:22:06 -0400

    highlighting, but cannot grey out roll

--- dice.js
+++ dice.js
@@ -1,3 +1,5 @@
+var totalRolls = 3;
+
 function rollDice(dice) {
 	$.each(dice, function() {
 		if(! $(this).hasClass("dieClicked")) {
@@ -6,23 +8,56 @@ function rollDice(dice) {
 	})
 }
 
+function resetDice(dice) {
+	$.each(dice, function() {
+		this.innerText = '';
+		$(this).removeClass('dieClicked');
+	});
+}
+
+function changePlayer(p1turn) {
+	var players = document.getElementsByClassName('player');
+	$.each(players, function() {
+		$(this).toggleClass('myTurn');
+	});
+	return !p1turn;
+}
+
 $(document).ready(function(){
 	var dice = document.getElementsByClassName('die');
+	var p1turn = true;
+	var numRolls = 0;
 
 	$("#roll").click(function() {
-		rollDice(dice);
+		if (numRolls < totalRolls) {
+			numRolls++;
+			console.log(numRolls);
+			console.log(p1turn);
+			rollDice(dice);
+		}
+		else {
+			console.log("here");
+			$("#roll").addClass("disable");
+			console.log(document.getElementsByClassName('disable'));
+		}
 	});
 	
 	$("#reset").click(function() {
-		$.each(dice, function() {
-			this.innerText = '';
-			$(this).removeClass('dieClicked');
-		});
+		resetDice(dice);
 	});
 
 	$(".die").click(function() {
 		if(this.innerText != '') {
 			$(this).toggleClass('dieClicked');
 		}
+	});
+
+	$("tr").click(function() {
+		// save score appropriately
+		console.log(this);
+		resetDice(dice);
+		p1turn = changePlayer(p1turn);
+		numRolls = 0;
+		$("#roll").removeClass("disable");
 	});
 });

99ebe1fef473bf03782c3a2218bc97b7593f232b
Kellen McDonnell <knmcdonn@colby.edu>
2017-06-06 15:27:13 -0400

    updated

---
+++ landing/clicks.js
@@ -0,0 +1,20 @@
+$(document).ready(function() {
+	$("#tripDesc").click(function() {
+		location.href = "http://google.com";
+	});
+	$("#announce").click(function() {
+		location.href = "http://google.com";
+	});
+	$("#atlas").click(function() {
+		location.href = "http://google.com";
+	});
+	$("#tripForm").click(function() {
+		location.href = "http://google.com";
+	});
+	$("#badgeForm").click(function() {
+		location.href = "http://google.com";
+	});
+	$("#mainEvDesc").click(function() {
+		location.href = "http://google.com";
+	});
+});

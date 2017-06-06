6d717450dc0d0f465e0a3d1051705e36d093efd5
Kellen McDonnell <knmcdonn@colby.edu>
2017-06-05 21:58:31 -0400

    add landing pages

---
+++ grid.js
@@ -0,0 +1,14 @@
+// external JS: masonry.pkgd.js
+
+var $grid = $('.grid').masonry({
+  itemSelector: '.grid-item',
+  columnWidth: 160
+  isAnimated: true
+});
+
+$grid.on( 'click', '.grid-item', function() {
+  // change size of item via class
+  $( this ).toggleClass('grid-item--gigante');
+  // trigger layout
+  $grid.masonry();
+});

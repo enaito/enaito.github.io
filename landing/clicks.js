9879f344ec386f3f82693bd688f343e722171f3a
Emily <emily.naito@gmail.com>
2017-06-07 11:23:06 -0400

    test

--- landing/clicks.js
+++ landing/clicks.js
@@ -1,20 +1,38 @@
 $(document).ready(function() {
-	$("#tripDesc").click(function() {
+	// Update this link every day
+	$("#announcements").click(function() {
 		location.href = "http://google.com";
 	});
-	$("#announce").click(function() {
-		location.href = "http://google.com";
-	});
+
+	// Update this link every day
 	$("#atlas").click(function() {
 		location.href = "http://google.com";
 	});
-	$("#tripForm").click(function() {
+
+	// Update this variable to have the text for the last box. If you want a line break, use "\n"
+	var newHeader = "Joke of the Day:"
+	var newContent= "Why don't kleptomaniacs understand puns?\n\nBecause they take everything literally!"
+
+	// Update this link once a week
+	$("#tripDescriptions").click(function() {
+		location.href = "http://google.com";
+	});
+
+	staticLinks(newHeader, newContent);
+});
+
+function staticLinks(newHeader, newContent) {
+	$("#tripSignUps").click(function() {
 		location.href = "http://google.com";
 	});
 	$("#badgeForm").click(function() {
 		location.href = "http://google.com";
 	});
-	$("#mainEvDesc").click(function() {
+	$("#mainEventDescriptions").click(function() {
 		location.href = "http://google.com";
 	});
-});
+	var header = document.createTextNode(newHeader);
+	var content = document.createTextNode(newContent);
+	document.getElementById("header").innerText = header.textContent;
+	document.getElementById("content").innerText = content.textContent;
+}
